8db64e47aa03f827fa471012e119871408c9de78
jcwitwicki <jc.witwicki@gmail.com>
2017-11-22 16:25:11 +0100

    role class

--- src/main/java/com/teamtreehouse/instateam/model/Role.java
+++ src/main/java/com/teamtreehouse/instateam/model/Role.java
@@ -1,5 +1,48 @@
 package com.teamtreehouse.instateam.model;
 
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String name;
+
+    @OneToMany(mappedBy = "role")
+    private List<Collaborator> collaborators = new ArrayList<>();
+
+    public Role() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Collaborator> getCollaborators() {
+        return collaborators;
+    }
+
+    public void setCollaborators(List<Collaborator> collaborators) {
+        this.collaborators = collaborators;
+    }
 }
