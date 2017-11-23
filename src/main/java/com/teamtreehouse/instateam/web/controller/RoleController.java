504ce823baf65e8a04f428cd80ca5e4e65c07ef5
jcwitwicki <jc.witwicki@gmail.com>
2017-11-23 21:47:46 +0100

    role controller added for viewing and adding roles

---
+++ src/main/java/com/teamtreehouse/instateam/web/controller/RoleController.java
@@ -0,0 +1,43 @@
+package com.teamtreehouse.instateam.web.controller;
+
+import com.teamtreehouse.instateam.model.Role;
+import com.teamtreehouse.instateam.service.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@Controller
+public class RoleController {
+
+    @Autowired
+    private RoleService roleService;
+
+    @RequestMapping("/roles")
+    public String listRoles(Model model) {
+        List<Role> roles = roleService.findAll();
+        if(!model.containsAttribute("role")){
+            model.addAttribute("role", new Role());
+        }
+        model.addAttribute("roles",roles);
+        return "role/roles";
+    }
+
+    @RequestMapping(value = "/roles", method = RequestMethod.POST)
+    public String addRole(@Valid Role role, BindingResult result, RedirectAttributes redirectAttributes) {
+        if (result.hasErrors()) {
+//            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.category", result);
+            redirectAttributes.addFlashAttribute("role", role);
+            return "redirect:/roles";
+        }
+        roleService.save(role);
+        return "redirect:/roles";
+    }
+
+}
