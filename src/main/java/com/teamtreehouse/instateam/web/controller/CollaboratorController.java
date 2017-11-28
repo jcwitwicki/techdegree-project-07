353131a8963b5c4ce9e6d12c760f4e555ad9a8d6
jcwitwicki <jc.witwicki@gmail.com>
2017-11-28 21:15:48 +0100

    Collaborator Controller

--- src/main/java/com/teamtreehouse/instateam/web/controller/CollaboratorController.java
+++ src/main/java/com/teamtreehouse/instateam/web/controller/CollaboratorController.java
@@ -6,8 +6,13 @@ import com.teamtreehouse.instateam.service.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -23,8 +28,31 @@ public class CollaboratorController {
     public String listCollaborators(Model model) {
         List<Collaborator> collaborators = collaboratorService.findAll();
         model.addAttribute("collaborators", collaborators);
+        if(!model.containsAttribute("collaborator")){
+            model.addAttribute("collaborator", new Collaborator());
+        }
         model.addAttribute("roles", roleService.findAll());
         return "collaborator/collaborators";
     }
+
+    @RequestMapping("collaborators/{collaboratorId}/edit")
+    public String editCollaborator(@PathVariable Long collaboratorId, Model model) {
+        if (!model.containsAttribute("collaborator")) {
+            model.addAttribute("collaborator", collaboratorService.findById(collaboratorId));
+        }
+        return "collaborator/edit_collaborator";
+    }
+
+    @RequestMapping(value = "/collaborators", method = RequestMethod.POST)
+    public String addCollaborator (@Valid Collaborator collaborator, BindingResult result, RedirectAttributes redirectAttributes) {
+        if (result.hasErrors()) {
+            redirectAttributes.addFlashAttribute("collaborator", collaborator);
+            return "redirect:/collaborators";
+        }
+        collaboratorService.save(collaborator);
+        return "redirect:/collaborators";
+    }
+
+
 
 }
