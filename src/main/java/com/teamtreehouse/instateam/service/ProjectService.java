0c3d179edb48c0282dfffb3445dc71ae6ae652e0
jcwitwicki <jc.witwicki@gmail.com>
2017-11-28 21:14:26 +0100

    Project Service

--- src/main/java/com/teamtreehouse/instateam/service/ProjectService.java
+++ src/main/java/com/teamtreehouse/instateam/service/ProjectService.java
@@ -7,7 +7,6 @@ import java.util.List;
 public interface ProjectService {
 
     void save(Project project);
-    void update(Project project);
     void delete(Project project);
     List<Project> findAll();
     Project findById(Long id);
