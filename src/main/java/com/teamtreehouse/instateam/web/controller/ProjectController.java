c583a08b2e2300d440ed8a5aa60c9c967d8b8dcc
jcwitwicki <jc.witwicki@gmail.com>
2017-11-28 21:15:21 +0100

    Project Controller

--- src/main/java/com/teamtreehouse/instateam/web/controller/ProjectController.java
+++ src/main/java/com/teamtreehouse/instateam/web/controller/ProjectController.java
@@ -5,6 +5,7 @@ import com.teamtreehouse.instateam.service.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -15,11 +16,21 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
-    @RequestMapping("/")
+    @RequestMapping({"/","/index"})
     public String listProjects(Model model) {
         List<Project> projects = projectService.findAll();
         model.addAttribute("projects", projects);
         return "project/index";
     }
+
+    @RequestMapping("/index/{projectId}")
+    public String project(@PathVariable Long projectId, Model model) {
+        if (!model.containsAttribute("project")) {
+            model.addAttribute("project", projectService.findById(projectId));
+        }
+        return "/project/project_detail";
+    }
+
+
 
 }
