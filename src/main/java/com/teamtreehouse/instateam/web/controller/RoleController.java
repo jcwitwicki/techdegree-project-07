7e32c2049cdb471e4a15e85dd5f806fed951ee2a
jcwitwicki <jc.witwicki@gmail.com>
2017-11-29 22:18:28 +0100

    Role controller

--- src/main/java/com/teamtreehouse/instateam/web/controller/RoleController.java
+++ src/main/java/com/teamtreehouse/instateam/web/controller/RoleController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -40,4 +41,21 @@ public class RoleController {
         return "redirect:/roles";
     }
 
+    @RequestMapping("/roles/{roleId}")
+    public String formEditRole (@PathVariable Long roleId, Model model) {
+        if (!model.containsAttribute("role")) {
+            model.addAttribute("role", roleService.findById(roleId));
+        }
+        model.addAttribute("action", String.format("/roles/%s",roleId));
+        return "role/edit";
+    }
+
+    @RequestMapping(value = "/roles/{roleId}", method = RequestMethod.POST)
+    public String updateRole (Role role) {
+        roleService.save(role);
+        return "redirect:/roles";
+    }
+
 }
+
+
