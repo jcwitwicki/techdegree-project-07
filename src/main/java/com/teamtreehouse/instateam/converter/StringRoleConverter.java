1bf34ea047b80c9646e6f4e4b4b1e12160856d4f
jcwitwicki <jc.witwicki@gmail.com>
2017-12-12 12:59:56 +0100

    Spring converter to converter String to Role

---
+++ src/main/java/com/teamtreehouse/instateam/converter/StringRoleConverter.java
@@ -0,0 +1,33 @@
+package com.teamtreehouse.instateam.converter;
+
+import com.teamtreehouse.instateam.dao.RoleDao;
+import com.teamtreehouse.instateam.model.Role;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.ConversionServiceFactoryBean;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Component
+public class StringRoleConverter implements Converter<String, Role> {
+
+    @Autowired
+    private RoleDao roleDao;
+
+    @Override
+    public Role convert(String source) {
+        Role role = roleDao.findById(new Long(source));
+        return role;
+    }
+
+    public ConversionService getConversionService() {
+        ConversionServiceFactoryBean bean = new ConversionServiceFactoryBean();
+        Set<Converter> converters = new HashSet<>();
+        converters.add(new StringRoleConverter());
+        bean.setConverters(converters);
+        return bean.getObject();
+    }
+}
