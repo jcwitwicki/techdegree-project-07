9ed889d9b54921c2fd70325106f41eae6957e97e
jcwitwicki <jc.witwicki@gmail.com>
2017-12-16 12:17:58 +0100

    Service Implementation

--- src/main/java/com/teamtreehouse/instateam/service/ProjectServiceImpl.java
+++ src/main/java/com/teamtreehouse/instateam/service/ProjectServiceImpl.java
@@ -1,6 +1,7 @@
 package com.teamtreehouse.instateam.service;
 
 import com.teamtreehouse.instateam.dao.ProjectDao;
+import com.teamtreehouse.instateam.model.Collaborator;
 import com.teamtreehouse.instateam.model.Project;
 import com.teamtreehouse.instateam.model.Role;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,4 +40,12 @@ public class ProjectServiceImpl implements ProjectService {
         List<Role> rolesNeeded = project.getRolesNeeded();
         return rolesNeeded;
     }
+
+    @Override
+    public List<Collaborator> findCollaborators(Project project) {
+        List<Collaborator> collaborators = project.getCollaborators();
+        return collaborators;
+    }
+
+
 }
