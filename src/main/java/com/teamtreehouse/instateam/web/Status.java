ae348d1e71efb8eceb70d79288919eb587c26417
jcwitwicki <jc.witwicki@gmail.com>
2018-01-10 21:58:21 +0100

    Enum status

--- src/main/java/com/teamtreehouse/instateam/web/Status.java
+++ src/main/java/com/teamtreehouse/instateam/web/Status.java
@@ -1,19 +1,30 @@
 package com.teamtreehouse.instateam.web;
 
 public enum Status {
-    ACTIVE("Active"),
-    ARCHIVED("Archived"),
-    NOT_STARTED("Not Started"),
-    TESTING("Testing");
+    ACTIVE("Active", "Green", "#72c38d"),
+    ARCHIVED("Archived", "Grey", "#bbbab9"),
+    NOT_STARTED("Not Started", "Blue", "#5976b3"),
+    TESTING("Testing", "Orange", "#b36859");
 
     private final String name;
+    private final String color;
+    private final String hexCode;
 
-    Status(String name) {
+    Status(String name, String color, String hexCode) {
         this.name = name;
+        this.color = color;
+        this.hexCode = hexCode;
     }
 
     public String getName() {
         return name;
     }
 
+    public String getColor() {
+        return color;
+    }
+
+    public String getHexCode() {
+        return hexCode;
+    }
 }
